bfc13b64a213d6f0059419f91cdd34d463ceda57
Brian Swan <bgswan@gmail.com>
2012-02-08 11:03:56 +0000

    Initial rails project with rspec

---
+++ upload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upload::Application.config.secret_token = '63a181abaeb3f3f992232140f5f628c4534c24d6661bf48ce6805172e95e690e031256030460e442e0f81b73f353737190b3e7fe3161bb39b5b99d18372e8ba2'
