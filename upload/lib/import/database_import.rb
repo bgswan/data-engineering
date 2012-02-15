1016559f3f17e8b5d14746041ddfdcfde29858a5
Brian Swan <bgswan@gmail.com>
2012-02-15 20:51:35 +0000

    Update to use attrs

--- upload/lib/import/database_import.rb
+++ upload/lib/import/database_import.rb
@@ -1,9 +1,11 @@
 class DatabaseImport
+
+  attr_reader :total_revenue_imported, :total_records_imported, :error
    
   def initialize(import_data) 
     @import_data = import_data
-    @total_revenue = 0
-    @total_records = 0
+    @total_revenue_imported = 0
+    @total_records_imported = 0
     @error = ""
   end
    
@@ -15,25 +17,13 @@ class DatabaseImport
           merchant = Merchant.find_or_create_by_name_and_address(r.merchant_name, r.merchant_address)
           item = Item.find_or_create_by_description_and_price_and_merchant_id(r.item_description, r.item_price, merchant.id)
           purchase = Purchase.create(:item => item, :purchaser => purchaser, :quantity => r.purchase_count)
-          @total_revenue += purchase.total
-          @total_records += 1
+          @total_revenue_imported += purchase.total
+          @total_records_imported += 1
         end
       end
     rescue StandardError => e
       @error = e.to_s
     end
-  end
-  
-  def total_revenue_imported
-    @total_revenue
-  end
-  
-  def total_records_imported
-    @total_records
-  end
-  
-  def error
-    @error
   end
 
 end
